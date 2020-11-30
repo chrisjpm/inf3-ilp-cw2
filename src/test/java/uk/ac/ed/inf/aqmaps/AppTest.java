db24b6a9871f473a77a9145ec139bd8e736b2ad1
C4R15 <chrispercevalmaxwell@gmail.com>
2020-11-30 14:06:21 +0000

    remved uneeded flights

--- src/test/java/uk/ac/ed/inf/aqmaps/AppTest.java
+++ src/test/java/uk/ac/ed/inf/aqmaps/AppTest.java
@@ -13,49 +13,45 @@ public class AppTest {
 	 */
 	@Test
 	public void testReadings() {
+		var yyyy = "2020";
 		var lat = "55.9444";
 		var lng = "-3.1878";
 		var seed = "5678";
 		var port = "80";
 		var counter = 0;
-				
-		for(int i=0; i < 2; i++) {
-			for(int j=1;j<13;j++) {
-				for(int k=1;k<13;k++) {
-					var yyyy = "";
-					var mm = "";
-					var dd = "";
-					
-					if(i==0) yyyy = "2020";
-					if(i==1) yyyy = "2021";				
-					
-					if(j==k) {
-						if(j<10) {
-							mm = "0" + Integer.toString(j);
-						} else {
-							mm = Integer.toString(j);
-						}
-						
-						if(k<10) {
-							dd = "0" + Integer.toString(k);
-						} else {
-							dd = Integer.toString(k);
-						}
-						
-						String[] input = {dd,mm,yyyy,lat,lng,seed,port};
-						App.main(input);
-						
-						counter++;
+
+		for (int j = 1; j < 13; j++) {
+			for (int k = 1; k < 13; k++) {
+				var mm = "";
+				var dd = "";
+
+				if (j == k) {
+					if (j < 10) {
+						mm = "0" + Integer.toString(j);
+					} else {
+						mm = Integer.toString(j);
 					}
-					
+
+					if (k < 10) {
+						dd = "0" + Integer.toString(k);
+					} else {
+						dd = Integer.toString(k);
+					}
+
+					String[] input = { dd, mm, yyyy, lat, lng, seed, port };
+					App.main(input);
+
+					counter++;
 				}
+
 			}
 		}
-		
-		System.out.println("\n*** TEST 1 *************************************");
-		System.out.println("Number of dates expected to test: 24");
+
+		System.out
+				.println("\n*** TEST 1 *************************************");
+		System.out.println("Number of dates expected to test: 12");
 		System.out.println("Actual number of dates tested: " + counter);
 		System.out.println("************************************************");
-		assertTrue(counter == 24);
+		assertTrue(counter == 12);
 	}
 }
